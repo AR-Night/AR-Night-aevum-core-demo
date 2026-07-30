df5789a40d3d47a1383baf3fe63f04fadef2c8d3
AR-Night <ricciardo.andrea@gmail.com>
2026-07-30 22:55:38 +0200

    Release Genesis 0.6 demo: cache split application files

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
-const CACHE='aevum-core-genesis-0-5-v1';
-const ASSETS=['./','./index.html','./iphone-preview.html','./guide.html','./manifest.webmanifest','./assets/icon.svg'];
+const CACHE='aevum-core-public-v0.6-split';
+const ASSETS=[
+  './',
+  './index.html',
+  './app.css',
+  './app-core.js',
+  './app-ui.js',
+  './app-demo.js',
+  './guide.html',
+  './iphone-preview.html',
+  './manifest.webmanifest',
+  './assets/icon.svg',
+  './README.md',
+  './COPYRIGHT.md'
+];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))))});
